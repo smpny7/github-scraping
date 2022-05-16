7fb683b965961e9901cd007a82eca1626ca2083e
smpny7 <git@coalabo.net>
2022-05-16 10:37:39 +0900

    feat: 🎸 取得したデータのインサート処理追加

---
+++ migrations/20220515180306-create-table-issue.js
@@ -0,0 +1,60 @@
+'use strict'
+
+var dbm
+var type
+var seed
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate
+  type = dbm.dataType
+  seed = seedLink
+}
+
+exports.up = function (db) {
+  db.createTable('issue', {
+    columns: {
+      url: 'string',
+      repository_url: 'string',
+      labels_url: 'string',
+      comments_url: 'string',
+      events_url: 'string',
+      html_url: 'string',
+      id: 'int',
+      node_id: 'string',
+      number: 'int',
+      title: 'text',
+      user_id: 'int',
+      state: 'string',
+      locked: 'boolean',
+      assignee_id: 'int',
+      milestone_id: 'int',
+      comments: 'int',
+      created_at: 'datetime',
+      updated_at: 'datetime',
+      closed_at: 'datetime',
+      author_association: 'string',
+      active_lock_reason: 'string',
+      draft: 'boolean',
+      pull_request_unique_key: 'string',
+      body: 'mediumtext',
+      reactions_unique_key: 'string',
+      timeline_url: 'string',
+      performed_via_github_app: 'string',
+    },
+    charset: 'utf8mb4',
+  })
+  return null
+}
+
+exports.down = function (db) {
+  db.dropTable('issue')
+  return null
+}
+
+exports._meta = {
+  version: 1,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,99 @@
 import { Octokit } from '@octokit/rest'
 import 'dotenv/config'
+import mysql from 'mysql2/promise'
+import { Issue } from './model/issue_model'
 
 const octokit = new Octokit({
   auth: `${process.env.GITHUB_TOKEN}`,
 })
 
-const getIssues = async (): Promise<void> => {
+function delay(n: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, n * 1000)
+  })
+}
+
+const getIssues = async (
+  page: number,
+  connection: mysql.Connection
+): Promise<void> => {
   const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
     owner: 'facebook',
     repo: 'react',
+    page: page,
+    per_page: 100,
+    state: 'all',
   })
 
-  console.log(response.data)
+  console.log(
+    `[Request limit: ${response.headers['x-ratelimit-remaining']} / ${response.headers['x-ratelimit-limit']}]`
+  )
+
+  const data: Issue[] = response.data as any
+
+  await insertIssue(data, connection)
 }
 
-getIssues()
+const insertIssue = async (
+  data: Issue[],
+  connection: mysql.Connection
+): Promise<void> => {
+  ;(async () => {
+    for (let issue of data) {
+      const row = {
+        id: issue.id,
+        url: issue.url,
+        repository_url: issue.repository_url,
+        labels_url: issue.labels_url,
+        comments_url: issue.comments_url,
+        events_url: issue.events_url,
+        html_url: issue.html_url,
+        node_id: issue.node_id,
+        number: issue.number,
+        title: issue.title,
+        user_id: issue.user?.id,
+        state: issue.state,
+        locked: issue.locked,
+        assignee_id: issue.assignee?.id,
+        milestone_id: issue.milestone?.id,
+        comments: issue.comments,
+        created_at:
+          issue.created_at === null
+            ? null
+            : new Date(issue.created_at).toLocaleString(),
+        updated_at:
+          issue.updated_at === null
+            ? null
+            : new Date(issue.updated_at).toLocaleString(),
+        closed_at:
+          issue.closed_at === null
+            ? null
+            : new Date(issue.closed_at).toLocaleString(),
+        author_association: issue.author_association,
+        active_lock_reason: issue.active_lock_reason,
+        draft: issue.draft,
+        pull_request_unique_key: issue.pull_request?.url,
+        body: issue.body,
+        reactions_unique_key: issue.reactions?.url,
+        timeline_url: issue.timeline_url,
+        performed_via_github_app: issue.performed_via_github_app,
+      }
+
+      await connection.query('INSERT INTO `issue` set ?', row)
+    }
+  })()
+}
+
+;(async () => {
+  const connection = await mysql.createConnection({
+    host: `${process.env.MYSQL_HOST}`,
+    user: `${process.env.MYSQL_USER}`,
+    password: `${process.env.MYSQL_PASSWORD}`,
+    database: `${process.env.MYSQL_DATABASE}`,
+  })
+
+  for (let num of [...Array(150)].map((_, i) => i)) {
+    await getIssues(num + 1, connection)
+    console.log(`${num + 1}/150 completed`)
+  }
+})()
